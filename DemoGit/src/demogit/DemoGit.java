e081f5f8f5f3c686f7938072238c90540d51cf9f
NgMinhHung <nguyenminhhung02082007@gmail.com>
2025-03-16 19:36:40 +0700

    Demo dua du an len GitHub

---
+++ DemoGit/src/demogit/DemoGit.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package demogit;
+
+/**
+ *
+ * @author MINH HUNG
+ */
+public class DemoGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Xin chào thầy");
+        System.out.println("Nhóm CoolGuy xin đc Demo");
+    }
+    
+}
